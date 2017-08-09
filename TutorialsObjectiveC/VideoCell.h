0d40c6e4748a13f1e60c101674f0e768d1e6af38
Jeff Ripke <jripke74@gmail.com>
2017-08-09 14:26:46 -0700

    add table view

---
+++ TutorialsObjectiveC/VideoCell.h
@@ -0,0 +1,13 @@
+//
+//  VideoCell.h
+//  TutorialsObjectiveC
+//
+//  Created by Jeff Ripke on 8/8/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoCell : UITableViewCell
+
+@end
